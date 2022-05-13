5b721ca397ac141d79a215a3d95fdf294ef816ca
LEEHYEOKJUN <kyuiop98@gmail.com>
2022-05-13 09:57:09 +0900

    delete

--- src/main/java/com/crowdsourcing/test/repository/FileMasterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.crowdsourcing.test.repository;
-
-import com.crowdsourcing.test.domain.FileMaster;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileMasterRepository extends JpaRepository<FileMaster, Long> {
-}
--- src/main/java/com/crowdsourcing/test/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.crowdsourcing.test.repository;
-
-import com.crowdsourcing.test.domain.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<File, Long>, CustomFileRepository {
-}
